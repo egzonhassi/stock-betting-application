e74719953a6dd86c2e9b1703a01aa3ed00de3de0
egzonhassi <egzon@bbros.eu>
2020-05-11 22:42:39 +0200

    Initial commit

---
+++ app/Http/Controllers/UserHasBetsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\UserHasBets;
+use Illuminate\Http\Request;
+
+class UserHasBetsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\UserHasBets  $userHasBets
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UserHasBets $userHasBets)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\UserHasBets  $userHasBets
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UserHasBets $userHasBets)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\UserHasBets  $userHasBets
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UserHasBets $userHasBets)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\UserHasBets  $userHasBets
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UserHasBets $userHasBets)
+    {
+        //
+    }
+}
